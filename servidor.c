7a5a2ea17fcc0ada6c54d39a002bc4ea785b9179
Diego <79831830+diegosanchezmartin@users.noreply.github.com>
2021-12-12 03:33:19 +0100

    Avances

--- servidor.c
+++ servidor.c
@@ -22,8 +22,7 @@
 
 #define PUERTO 5121
 #define ADDRNOTFOUND	0xffffffff	/*Dirección de retorno de host no encontrado.*/
-#define BUFFERSIZE	1024	/*Tamaño máximo de los paquetes recibidos.*/
-#define TAM_BUFFER 10
+#define BUFFERSIZE 1024	/*Tamaño máximo de los paquetes recibidos.*/
 #define MAXHOST 128
 
 void serverTCP(int s, struct sockaddr_in peeraddr_in);
@@ -34,7 +33,7 @@ int FIN = 0;
 void finalizar(){ FIN =1; }
 
 
-int main(int argc, char * argv){
+int main(int argc, char *argv[]){
 
     int s_TCP, s_UDP;   /*Descriptor de socket conectado*/
     int ls_TCP;         /*Descriptor del listen socket (TCP)*/
@@ -211,7 +210,125 @@ int main(int argc, char * argv){
 
 
 void serverTCP(int s, struct sockaddr_in clientaddr_in){
+  int reqcnt = 0;		/* keeps count of number of requests */
+  char buf[BUFFERSIZE];		/* This example uses BUFFERSIZE byte messages. */
+  char hostname[MAXHOST];		/* remote host's name string */
 
+  int len, len1, status;
+    struct hostent *hp;		/* pointer to host info for remote host */
+    long timevar;			/* contains time returned by time() */
+
+    struct linger linger;		/* allow a lingering, graceful close; */
+                        /* used when setting SO_LINGER */
+
+  /* Look up the host information for the remote host
+   * that we have connected with.  Its internet address
+   * was returned by the accept call, in the main
+   * daemon loop above.
+   */
+
+     status = getnameinfo((struct sockaddr *)&clientaddr_in,sizeof(clientaddr_in),
+                           hostname,MAXHOST,NULL,0,0);
+     if(status){
+            /* The information is unavailable for the remote
+       * host.  Just format its internet address to be
+       * printed out in the logging information.  The
+       * address will be shown in "internet dot format".
+       */
+       /* inet_ntop para interoperatividad con IPv6 */
+            if (inet_ntop(AF_INET, &(clientaddr_in.sin_addr), hostname, MAXHOST) == NULL)
+              perror(" inet_ntop \n");
+             }
+    /* Log a startup message. */
+    time (&timevar);
+    /* The port number must be converted first to host byte
+     * order before printing.  On most hosts, this is not
+     * necessary, but the ntohs() call is included here so
+     * that this program could easily be ported to a host
+     * that does require it.
+     */
+     printf("Startup from %s port %u at %s", hostname, ntohs(clientaddr_in.sin_port), (char *) ctime(&timevar));
+
+    /* Set the socket for a lingering, graceful close.
+     * This will cause a final close of this socket to wait until all of the
+     * data sent on it has been received by the remote host.
+     */
+    linger.l_onoff  =1;
+    linger.l_linger =1;
+    if (setsockopt(s, SOL_SOCKET, SO_LINGER, &linger,
+            sizeof(linger)) == -1) {
+      errout(hostname);
+    }
+
+    /* Go into a loop, receiving requests from the remote
+     * client.  After the client has sent the last request,
+     * it will do a shutdown for sending, which will cause
+     * an end-of-file condition to appear on this end of the
+     * connection.  After all of the client's requests have
+     * been received, the next recv call will return zero
+     * bytes, signalling an end-of-file condition.  This is
+     * how the server will know that no more requests will
+     * follow, and the loop will be exited.
+     */
+  while (len = recv(s, buf, BUFFERSIZE, 0)) {
+    if (len == -1) errout(hostname); /* error from recv */
+      /* The reason this while loop exists is that there
+       * is a remote possibility of the above recv returning
+       * less than BUFFERSIZE bytes.  This is because a recv returns
+       * as soon as there is some data, and will not wait for
+       * all of the requested data to arrive.  Since BUFFERSIZE bytes
+       * is relatively small compared to the allowed TCP
+       * packet sizes, a partial receive is unlikely.  If
+       * this example had used 2048 bytes requests instead,
+       * a partial receive would be far more likely.
+       * This loop will keep receiving until all BUFFERSIZE bytes
+       * have been received, thus guaranteeing that the
+       * next recv at the top of the loop will start at
+       * the begining of the next request.
+       */
+    printf("%s ", buf);
+    }
+
+
+      /* Increment the request count. */
+    reqcnt++;
+      /* This sleep simulates the processing of the
+       * request that a real server might do.
+       */
+
+
+
+    sleep(1);
+
+
+
+
+      /* Send a response back to the client. */
+    if (send(s, buf, BUFFERSIZE, 0) != BUFFERSIZE) errout(hostname);
+
+
+    /* The loop has terminated, because there are no
+     * more requests to be serviced.  As mentioned above,
+     * this close will block until all of the sent replies
+     * have been received by the remote host.  The reason
+     * for lingering on the close is so that the server will
+     * have a better idea of when the remote has picked up
+     * all of the data.  This will allow the start and finish
+     * times printed in the log file to reflect more accurately
+     * the length of time this connection was used.
+     */
+  close(s);
+
+    /* Log a finishing message. */
+  time (&timevar);
+    /* The port number must be converted first to host byte
+     * order before printing.  On most hosts, this is not
+     * necessary, but the ntohs() call is included here so
+     * that this program could easily be ported to a host
+     * that does require it.
+     */
+  printf("Completed %s port %u, %d requests, at %s\n",
+    hostname, ntohs(clientaddr_in.sin_port), reqcnt, (char *) ctime(&timevar));
 }
 
 
